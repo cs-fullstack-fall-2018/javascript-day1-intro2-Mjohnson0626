22a8b7011781abb2cc8a162157b79e8fa5350ef3
Mike <m.g.johnson0626@gmail.com>
2018-07-30 12:16:43 -0500

    final

--- EX5.js
+++ EX5.js
@@ -1,5 +1,5 @@
 var bal = prompt("Enter your balance");
-console.log("I have granyed you $50 more");
-console.log(bal);
+console.log("I have granted you $50 more");
+console.log(bal + " Is your balance");
 var newBal = bal + 50;
 console.log("Your new balance is: " + newBal);
